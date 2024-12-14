c2dbb249c1d4effb2e9f2f39d47e2cf151c3503a
minnie <radic7700@gmail.com>
2024-12-14 19:44:44 +0900

    feat: user 등록 기능 개발

--- src/main/java/com/cloudhub/barovillage/domain/user/UserService.java
+++ src/main/java/com/cloudhub/barovillage/domain/user/UserService.java
@@ -3,6 +3,7 @@ package com.cloudhub.barovillage.domain.user;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -10,4 +11,13 @@ import org.springframework.stereotype.Service;
 public class UserService {
     private final UserRepository userRepository;
     private final UserLocationRepository userLocationRepository;
+
+    // 회원 등록
+    @Transactional
+    public User signUp() {
+        User savedUser = userRepository.save(new User());
+
+        return savedUser.setNickname("유저" + savedUser.getId());
+
+    }
 }
