6046c45cba3c8f660a0e8960e2a11b743190e78b
minnie <radic7700@gmail.com>
2024-12-14 17:25:05 +0900

    add .gitignore

---
+++ src/main/java/com/cloudhub/barovillage/BarovillageApplication.java
@@ -0,0 +1,13 @@
+package com.cloudhub.barovillage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarovillageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarovillageApplication.class, args);
+	}
+
+}
